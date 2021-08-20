a69955462df454fe50a1128c755370bc2bba1f4d
RadmirShkred <psitecton@gmail.com>
2021-08-20 13:34:35 +0300

    feat: add desicion

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.start = 0;
+        this.end = 0;
+        this.number = 0;
+    }
 
-    setRange(min, max) {
-
+    setRange(start, end) {
+        this.start = start;
+        this.end = end;
     }
 
     guess() {
-
+        this.number = Math.round((this.start + this.end) / 2);
+        return this.number;
     }
 
     lower() {
-
+        this.end = this.number;
     }
 
     greater() {
-
+        this.start = this.number;
     }
 }
 
